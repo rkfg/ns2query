3ad0582b5ca94f6826a821aa7bcaa33a74f65462
rkfg <rkfg@rkfg.me>
2023-03-07 12:56:20 +0300

    Update skill data source

--- hive.go
+++ hive.go
@@ -3,42 +3,108 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/Philipp15b/go-steamapi"
 	"github.com/bwmarrin/discordgo"
 )
 
 type hive struct {
-	Alias           string
-	Skill           int
-	SkillOffset     int `json:"skill_offset"`
-	CommSkill       int `json:"comm_skill"`
-	CommSkillOffset int `json:"comm_skill_offset"`
+	GameName    string
+	Playerstats struct {
+		Stats []struct {
+			Name  string
+			Value float64
+		}
+	}
 }
 
 func getSkill(playerID uint32) (*discordgo.MessageSend, error) {
-	url := fmt.Sprintf("http://hive2.ns2cdt.com/api/get/playerData/%d", playerID)
+	steamID := uint64(playerID) + 0x110000100000000
+	url := fmt.Sprintf("https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2/?key=%s&steamid=%d&appid=4920", config.SteamKey, steamID)
 	hiveResp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
-	var skill hive
-	json.NewDecoder(hiveResp.Body).Decode(&skill)
-	if skill.Alias == "" {
-		return nil, fmt.Errorf("player id %d isn't present on Hive", playerID)
+	var steamData hive
+	json.NewDecoder(hiveResp.Body).Decode(&steamData)
+	if steamData.GameName == "" {
+		summary, err := steamapi.GetPlayerSummaries([]uint64{steamID}, config.SteamKey)
+		if err != nil || len(summary) == 0 {
+			log.Printf("Error getting player info: %s", err)
+			steamData.GameName = "<err>"
+		} else {
+			steamData.GameName = summary[0].PersonaName
+		}
+	}
+	skill := [4]int{}
+	skillOffset := [4]int{}
+	signs := [4]bool{}
+	for _, s := range steamData.Playerstats.Stats {
+		switch s.Name {
+		case "skill":
+			skill[0] = int(s.Value)
+		case "skill_offset":
+			skillOffset[0] = int(s.Value)
+		case "comm_skill":
+			skill[1] = int(s.Value)
+		case "comm_skill_offset":
+			skillOffset[1] = int(s.Value)
+		case "td_skill":
+			skill[2] = int(s.Value)
+		case "td_skill_offset":
+			skillOffset[2] = int(s.Value)
+		case "td_comm_skill":
+			skill[3] = int(s.Value)
+		case "td_comm_skill_offset":
+			skillOffset[3] = int(s.Value)
+		case "skill_offset_sign":
+			if int(s.Value) == 1 {
+				signs[0] = true
+			}
+		case "comm_skill_offset_sign":
+			if int(s.Value) == 1 {
+				signs[1] = true
+			}
+		case "td_skill_offset_sign":
+			if int(s.Value) == 1 {
+				signs[2] = true
+			}
+		case "td_comm_skill_offset_sign":
+			if int(s.Value) == 1 {
+				signs[3] = true
+			}
+		}
+	}
+	for i, s := range signs {
+		if s {
+			skillOffset[i] = -skillOffset[i]
+		}
 	}
 	return &discordgo.MessageSend{Embed: &discordgo.MessageEmbed{
 		Description: "Skill breakdown",
-		Author:      &discordgo.MessageEmbedAuthor{Name: skill.Alias, IconURL: getPlayerAvatar(playerID)},
+		Author:      &discordgo.MessageEmbedAuthor{Name: steamData.GameName, IconURL: getPlayerAvatar(playerID)},
 		Fields: []*discordgo.MessageEmbedField{
 			{
 				Name:   "Marine (field/comm)",
-				Value:  fmt.Sprintf("%d/%d", skill.Skill+skill.SkillOffset, skill.CommSkill+skill.CommSkillOffset),
+				Value:  fmt.Sprintf("%d/%d", skill[0]-skillOffset[0], skill[1]-skillOffset[1]),
 				Inline: true,
 			},
 			{
 				Name:   "Alien (field/comm)",
-				Value:  fmt.Sprintf("%d/%d", skill.Skill-skill.SkillOffset, skill.CommSkill-skill.CommSkillOffset),
+				Value:  fmt.Sprintf("%d/%d", skill[0]+skillOffset[0], skill[1]+skillOffset[1]),
+				Inline: true,
+			},
+			{},
+			{
+				Name:   "TD Marine (field/comm)",
+				Value:  fmt.Sprintf("%d/%d", skill[2]-skillOffset[2], skill[3]-skillOffset[3]),
+				Inline: true,
+			},
+			{
+				Name:   "TD Alien (field/comm)",
+				Value:  fmt.Sprintf("%d/%d", skill[2]+skillOffset[2], skill[3]+skillOffset[3]),
 				Inline: true,
 			},
 		},
